f483ebcd55c110c696e1ff31053fc1e59eb2c0cf
Felix Vicente <felixvicent1306@gmail.com>
2022-01-02 16:43:38 -0300

    Pass param by constructor

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,13 +1,18 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  const CategoriesMealsScreen(this.category);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Receita'),
+        title: Text(category.title),
       ),
       body: Center(
         child: Text('Receitas por categorias'),
