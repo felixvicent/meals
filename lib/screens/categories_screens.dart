049a41e5db8100c22cd4a55738478d74acb77a15
Felix Vicente <felixvicent1306@gmail.com>
2022-01-02 16:06:04 -0300

    Create categories model

---
+++ lib/screens/categories_screens.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: [],
+    );
+  }
+}
