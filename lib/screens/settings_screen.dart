2537344a9da8074947606903d9ed502be6b5f5fa
Felix Vicente <felixvicent1306@gmail.com>
2022-01-03 16:30:24 -0300

    Fix drawer navigation

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:meals/components/main_drawer.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -9,6 +10,7 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Configurações'),
       ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text(
           'Configurações',
